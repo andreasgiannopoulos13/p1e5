e1b1e55ef14e37394ed57537366130aed5ca398e
andreasgiannopoulos13 <73398216+andreasgiannopoulos13@users.noreply.github.com>
2020-11-22 19:03:31 +0200

    Add files via upload

---
+++ telestes.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+
+int main (void)
+{ int a=4,b=3,c=1
+a += b -= ++ c * a--;
+printf("%d%d%d\n",a,b,c);
+return 0;
+\* h eksigisi tou apotelesmatos twn timwn pou pairnoyn ta a,b,c einai kanontas tis praxeis bgainei to apotelesma xekinontas apo ton pollaplasiasmo 
+kai meta tis upoloipes
+}
