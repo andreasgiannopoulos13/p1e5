d44ff34363be121ba235b51ac50fdbc44619818f
andreasgiannopoulos13 <73398216+andreasgiannopoulos13@users.noreply.github.com>
2020-11-22 14:06:21 +0200

    Add files via upload

---
+++ compare.c
@@ -0,0 +1,23 @@
+#include <studio.h>
+
+int main (void)
+
+{
+	double c,d;
+	 
+	 printf ("enter numbers:");
+	 scanf ("%1f",&c);
+	 
+	 printf("enter numbers:");
+	 scanf ("%1f",&d);
+	 
+	 if (c<d){
+	 	printf("o mikroteros arithmos einai = %1f,\n",c);
+	 }
+	 
+	 if (c>d){
+	 	printf("o mikroteros arithmos einai = %1f,\n",d);
+	 }
+	 
+	 return 0;
+}
