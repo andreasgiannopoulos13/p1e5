806b726852141ffe4cc00eee098685fdd5120a82
andreasgiannopoulos13 <73398216+andreasgiannopoulos13@users.noreply.github.com>
2020-11-22 18:24:58 +0200

    Add files via upload

---
+++ logical.c
@@ -0,0 +1,22 @@
+#include <studio.h>
+
+int main (void) {
+	scanf("%d%d",&a,&b);
+	if(a<0 || b>10)
+	{
+		printf("lathos bathmos a");
+	
+	}
+	if(b<0 || b>10 )
+	{
+		printf("lathos batmos b");
+	
+	}
+	if (a>5&& b>5)
+	
+	{
+		printf("perases to mathima");
+	
+	}
+	return 0;
+}
