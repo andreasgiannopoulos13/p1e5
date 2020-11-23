4d5bfcb4a6de57bc5b6925cbf3c606f3ca27cfa4
andreasgiannopoulos13 <73398216+andreasgiannopoulos13@users.noreply.github.com>
2020-11-23 20:04:22 +0200

    Add files via upload

---
+++ reverse.c
@@ -0,0 +1,15 @@
+#include <studio.h>
+
+int main (void)
+{
+	double a,b;
+	printf("dose dyo arnitikous arithmoys");
+	scanf ("%1f %1f", &a , &b);
+	if (a, b < 0 ) {
+		printf("sfalma");
+		
+	}
+	printf ("%1f %1f", -a ,-b );
+	return 0;
+}
+
